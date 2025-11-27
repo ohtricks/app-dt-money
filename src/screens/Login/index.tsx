c3b0253b544138b996989fb5f541cd7985780ae4
Rafael Reis <rafael.reis@leadersoft.com.br>
2025-11-27 09:55:50 -0300

    initial commit

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+export const Login = () => {
+    return (
+        <View className="flex-1 justify-center items-center bg-green-950">
+            <Text>
+                Login
+            </Text>
+        </View>
+    )
+}
